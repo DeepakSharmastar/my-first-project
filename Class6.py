f7b77f2c03d2f3391e3d357d721e94082012b1d9
Your Name <youremail@gmail.com>
2026-01-28 22:32:29 +0530

    my new file class6.py

--- Class6.py
+++ Class6.py
@@ -65,25 +65,65 @@
 
 # 3. WAF to find the factorial on n (n is perameter)
 
-def cal_fact(n):
-    fact =1
-    for i in range(1,n+1):
-     fact *= i
-    print(fact)
-cal_fact(5)
+# def cal_fact(n):
+#     fact =1
+#     for i in range(1,n+1):
+#      fact *= i
+#     print(fact)
+# cal_fact(5)
 
 # 4. wap to convert usd to inr
 
-def converter(usd_val):
-   inr_val = usd_val * 83
-   print(usd_val, "USD = ", inr_val)
+# def converter(usd_val):
+#    inr_val = usd_val * 83
+#    print(usd_val, "USD = ", inr_val)
 
-converter(2)
+# converter(2)
 
-def oddeven(n):
-   if(n%2==0):
-      print("even")
-   else:
-    print("odd")
+# def oddeven(n):
+#    if(n%2==0):
+#       print("even")
+#    else:
+#     print("odd")
    
-oddeven(10)
+# oddeven(10)
+
+# Recusrion 
+# def show(n):
+#    if(n==0):  #base case for stop recursion
+#       return
+#    print(n)
+#    show(n-1)
+
+# show(5)
+
+# def fact(n):
+#     if(n==0 or n==1): 
+#         return 1
+    
+#     return fact(n-1)*n
+# print(fact(4))
+
+# wap a recursive function to calculate the sum of first n natural numbers
+
+# def first_natural(n):
+#     if(n==0):
+#         return 0
+#    #  print(n)
+#     return first_natural(n-1) + n
+# sum = first_natural(5)
+# print(sum)
+
+# wap a recursive function to print all elements in a lsit
+# hint : use list & index as parameters 
+
+def print_list(list , idx=0):
+    if(idx == len(list)):
+        return 
+    print(list[idx])
+    print_list(list,idx + 1)
+
+fruits = ["mango", "banana" , "apple"]
+print_list(fruits)
+
+    
