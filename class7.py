76090d74f32bb542814b04a3ec1ab134dcd3b19f
Your Name <youremail@gmail.com>
2026-01-30 22:44:31 +0530

    some new changes

--- class7.py
+++ class7.py
@@ -111,14 +111,70 @@ import os
 
 # convert in function 
 
-def check_for_ford():
-    word  = "learning"
-    with open("practice.txt","r") as f:
-      data = f.read()
-      if(data.find(word)!= -1):
-        print("found")
-      else:
-        print("Not Found")
+# def check_for_ford():
+#     word  = "learning"
+#     with open("practice.txt","r") as f:
+#       data = f.read()
+#       if(data.find(word)!= -1):
+#         print("found")
+#       else:
+#         print("Not Found")
 
 
-check_for_ford()
+# check_for_ford()
+
+# wap to find in which line of the file does the word "learning" occure first.
+#print -1 if word not found
+
+# def check_for_ford():
+#     word  = "learning"
+#     with open("practice.txt","r") as f:
+#       data = f.read()
+#       if(data.find(word)!= -1):
+#         print("found")
+#       else:
+#         print("Not Found")
+
+
+# check_for_ford()
+
+# def check_for_line():
+#    word = "learning"
+#    data = True
+#    line_no = 1
+#    with open("practice.txt","r") as f:
+#       while data:
+#          data = f.readline()
+#          if(word in data):
+#             # print(line_no)
+#             return line_no
+#          line_no += 1
+#    return -1
+        
+
+# print(check_for_line())
+
+# from a file contaning numbers separated by comma, print the count of even numbers
+# with open("practice.txt","r") as f:
+#     data = f.read()
+#     print(data)
+
+#     num = ""
+#     for i in range(len(data)):
+#         if(data[i] == ","):
+#             print(int(num))
+#             num = ""
+#         else:
+#             num += data[i]
+
+count = 0
+with open("practice.txt","r") as f:
+    data = f.read()
+    print(data)
+
+    nums = data.split(",")
+    for val in nums:
+        if(int(val)%2==0):
+            print(val)
+            count += 1
+print(count)
