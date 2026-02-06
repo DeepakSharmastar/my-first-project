cdc6f85bb1c385813b23fbbaa6ee910926e55f42
Your Name <youremail@gmail.com>
2026-02-06 21:28:57 +0530

    new class 9

---
+++ class9.py
@@ -0,0 +1,13 @@
+# inheritence 
+# polymorophism
+
+# del keyword (used to delete object properties of object itself. )
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+s1 = Student("deepak")
+
+del(s1)
+print("vfvdv")
